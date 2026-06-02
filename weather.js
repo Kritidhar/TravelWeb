ec28157bda13fa2754e4024185bfbb73a5062971
Kriti Dhar <123760393+Kritidhar@users.noreply.github.com>
2026-06-02 18:50:15 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,62 @@
+
+const apiKey = "";
+document
+  .getElementById("weatherForm")
+  .addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    const people = document.getElementById("people").value;
+    const city = document.getElementById("city").value.trim();
+    const date = document.getElementById("date").value;
+
+    const resultDiv = document.getElementById("result");
+
+    // FETCHING FROM WEATHER OPENAPI
+    resultDiv.style.display = "block";
+    resultDiv.innerHTML = "<p>Fetching weather...</p>";
+
+    try {
+
+       const response = await fetch(
+    `/api/config?location=${location}`);
+
+    if(!response.ok){
+            throw new Error("API Request Failed");
+        }
+
+      const data = await response.json();
+
+      // RESULT 
+      resultDiv.innerHTML = `
+        <h2>🌤 Weather in ${data.name}</h2>
+
+        <div class="weather-card">
+          <p><strong>👥 Travellers:</strong> ${people}</p>
+          <p><strong>📅 Arrival Date:</strong> ${date}</p>
+          <p><strong>🌡 Temperature:</strong> ${data.main.temp}°C</p>
+          <p><strong>☁ Condition:</strong> ${data.weather[0].description}</p>
+          <p><strong>💧 Humidity:</strong> ${data.main.humidity}%</p>
+          <p><strong>💨 Wind Speed:</strong> ${data.wind.speed} m/s</p>
+
+          <hr>
+
+          <h3> Travel Suggestions </h3>
+          <p>✓ Carry a water bottle</p>
+          <p>✓ Check local attractions before travelling</p>
+          <p>✓ Pack clothes according to weather conditions</p>
+        </div>
+      `;
+    // ERROR HANDLING
+
+    } catch (error) {
+
+      console.error(error);
+
+      resultDiv.innerHTML = `
+        <h2> Error</h2>
+        <p>${error.message}</p>
+      `;
+    }
+
+});
